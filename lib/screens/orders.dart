43f7f6efa08ddb6db6491cf2402d0aa005c4e350
AdrianIchiro <adrianfahren10@gmail.com>
2024-12-19 02:05:46 +0700

    membuat implementasi order di halaman orders

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import '../api/auth_service.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Orders extends StatefulWidget {
   const Orders({super.key});
@@ -8,10 +13,78 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
-  String selectedTab = 'Delivered';
+  String selectedTab = 'paid';
+  List<dynamic> orders = [];
+  bool isLoading = true;
+  final FlutterSecureStorage storage = FlutterSecureStorage();
+  final AuthService _authService = AuthService();
+  String id = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeProfile();
+    fetchOrders();
+  }
+
+  void _initializeProfile() async {
+    final result = await _authService.decodeProfile(context);
+    setState(() {
+      id = result['id'];
+    });
+  }
+
+  Future<void> fetchOrders() async {
+    final String? baseUrl = dotenv.env['BASE_URL'];
+    final String apiUrl = '$baseUrl/order/$id';
+    final token = await storage.read(key: 'accessToken');
+
+    try {
+      final response = await http.get(
+        Uri.parse(apiUrl),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print("Response data: $data");
+
+        if (data != null && data is List) {
+          setState(() {
+            orders = data;
+            isLoading = false;
+          });
+        } else {
+          setState(() {
+            orders = [];
+            isLoading = false;
+          });
+        }
+      } else {
+        throw Exception('Failed to load orders');
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print('Error fetching orders: $e');
+    }
+  }
+
+  void updateTab(String status) {
+    setState(() {
+      selectedTab = status;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<dynamic> filteredOrders =
+        orders.where((order) => order['status'] == selectedTab).toList();
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -35,43 +108,38 @@ class _OrdersState extends State<Orders> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 TabButton(
-                  title: 'Delivered',
-                  selected: selectedTab == 'Delivered',
-                  onTap: () {
-                    setState(() {
-                      selectedTab = 'Delivered';
-                    });
-                  },
+                  title: 'Paid',
+                  selected: selectedTab == 'paid',
+                  onTap: () => updateTab('paid'),
                 ),
                 TabButton(
-                  title: 'Processing',
-                  selected: selectedTab == 'Processing',
-                  onTap: () {
-                    setState(() {
-                      selectedTab = 'Processing';
-                    });
-                  },
-                ),
-                TabButton(
-                  title: 'Cancelled',
-                  selected: selectedTab == 'Cancelled',
-                  onTap: () {
-                    setState(() {
-                      selectedTab = 'Cancelled';
-                    });
-                  },
+                  title: 'Pending',
+                  selected: selectedTab == 'pending',
+                  onTap: () => updateTab('pending'),
                 ),
               ],
             ),
             const SizedBox(height: 16),
-            Expanded(
-              child: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return OrderCard(status: selectedTab);
-                },
-              ),
-            ),
+            isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : filteredOrders.isEmpty
+                    ? const Center(child: Text("No orders available"))
+                    : Expanded(
+                        child: ListView.builder(
+                          itemCount: filteredOrders.length,
+                          itemBuilder: (context, index) {
+                            final order = filteredOrders[index];
+                            return OrderCard(
+                              status: order['status'],
+                              orderId: order['order_id'],
+                              createdAt: order['createdAt'],
+                              quantity: order['ordersItem'][0]['quantity'],
+                              totalPrice: order['ordersItem'][0]['total_price'],
+                              courier: order['shipping']['courier'],
+                            );
+                          },
+                        ),
+                      ),
           ],
         ),
       ),
@@ -84,11 +152,12 @@ class TabButton extends StatelessWidget {
   final bool selected;
   final VoidCallback onTap;
 
-  const TabButton(
-      {super.key,
-      required this.title,
-      required this.selected,
-      required this.onTap});
+  const TabButton({
+    super.key,
+    required this.title,
+    required this.selected,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -117,8 +186,21 @@ class TabButton extends StatelessWidget {
 
 class OrderCard extends StatelessWidget {
   final String status;
+  final String orderId;
+  final String createdAt;
+  final int quantity;
+  final double totalPrice;
+  final String courier;
 
-  const OrderCard({super.key, required this.status});
+  const OrderCard({
+    super.key,
+    required this.status,
+    required this.orderId,
+    required this.createdAt,
+    required this.quantity,
+    required this.totalPrice,
+    required this.courier,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -131,40 +213,43 @@ class OrderCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'Order 1947034',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+                Flexible(
+                  child: Text(
+                    'Order ID: $orderId',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
                 Text(
-                  '05-12-2019',
-                  style: TextStyle(color: Colors.grey),
+                  createdAt.substring(0, 10),
+                  style: const TextStyle(color: Colors.grey),
                 ),
               ],
             ),
             const SizedBox(height: 8),
-            const Text('Tracking number: IW3475453455'),
+            Text('Courier: $courier'),
             const SizedBox(height: 4),
-            const Text('Quantity: 3'),
+            Text('Quantity: $quantity'),
             const SizedBox(height: 4),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
-                  'Total Amount: 112\$',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                Text(
+                  'Total Amount: ${totalPrice.toStringAsFixed(2)}\$',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 Text(
                   status,
                   style: TextStyle(
-                    color: status == 'Delivered'
+                    color: status == 'paid'
                         ? Colors.green
-                        : status == 'Processing'
+                        : status == 'pending'
                             ? Colors.orange
                             : Colors.red,
                     fontWeight: FontWeight.bold,
@@ -182,7 +267,7 @@ class OrderCard extends StatelessWidget {
                     borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-                child: Text('Details'),
+                child: const Text('Details'),
               ),
             ),
           ],
